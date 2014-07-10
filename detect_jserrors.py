1b698a7f6f2cde1e0770485a681de83bd6557f77
Gary Kwong <gary@rumblingedge.com>
2014-07-09 17:03:10 -0700

    Initial landing of marifuzz which has been tested to work on a Flame developer device and Firefox OS 2.1 nightly. Adapted from a Jan 2014 PoC for Firefox OS 1.2 and 1.3.

---
+++ detect_jserrors.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+#from collections import OrderedDict
+#list(OrderedDict.fromkeys(items))
+
+
+class Known(object):
+    def __init__(self):
+        self.interestingJsErrors = []
+        self.interestingJsWarnings = []
+
+    def read_known_file(self):
+        '''
+        Returns a list of known errors excluding comments.
+        '''
+        knownErrors = []
+        with open(os.path.expanduser(os.path.join(os.pardir, os.pardir, 'known',
+                                                     'mozilla-central', 'jserrors.txt')), 'rb') as f:
+            contentsF = f.readlines()
+            for line in contentsF:
+                if not line.startswith('#'):
+                    knownErrors.append(line)
+
+        return knownErrors
+
+    def analyse_log_file(self):
+        '''
+        Analyses the log files and appends interesting lines when found.
+        '''
+        outfile = os.path.expanduser(os.path.join('out.txt'))
+        with open(outfile, 'rb') as f:
+            contentsF = f.readlines()
+            for line in contentsF:
+                # Gaia and Gecko problems appear here
+                if 'Gecko' in line and '[' in line:
+                    excludeLeftPart = line.split('[')[1]
+                    errorMsg = excludeLeftPart.split(' line: ')[0]
+                    if 'backgroud' in line:
+                        import pdb; pdb.set_trace()
+                    if 'JavaScript error'.lower() in line.lower() and errorMsg not in str(self.read_known_file()) and line.rstrip() not in self.interestingJsErrors:
+                        self.interestingJsErrors.append(line.rstrip())
+                    elif 'JavaScript warning'.lower() in line.lower() and errorMsg not in str(self.read_known_file()) and line.rstrip() not in self.interestingJsWarnings:
+                        self.interestingJsWarnings.append(line.rstrip())
+
+
+def main():
+    kn = Known()
+    kn.analyse_log_file()
+
+    print '\n Total interesting error count: ' + str(len(kn.interestingJsErrors + kn.interestingJsWarnings))
+
+    print '\n Interesting JavaScript error count: ' + str(len(kn.interestingJsErrors))
+    for line in kn.interestingJsErrors:
+        print line
+    print
+
+    print '\n Total JavaScript warning count: ' + str(len(kn.interestingJsWarnings))
+    for line in kn.interestingJsWarnings:
+        print line
+    print
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_sample_fuzz.py
@@ -0,0 +1,117 @@
+# Works with 1.2 and v1.3:
+# $ . ~/trees/virtualenv-b2g/bin/activate
+# (in a virtualenv) $ rlwrap gaiatest --address=localhost:2828 --testvars=testVars.json --restart test_sample_fuzz.py
+# $ clear && grep -i "JavaScript Error" out.txt | sort | uniq -c | sort -n -r
+
+# Idea for generational: https://marionette_client.readthedocs.org/en/latest/
+# Find list of tappable IDs, then tap a random ID in each app
+
+# Idea for mutation: https://github.com/mozilla-b2g/gaia/blob/master/tests/python/gaia-ui-tests/gaiatest/tests/endurance/test_endurance_settings.py
+# overwrite settings function within the TestEnduranceSettings object to navigate to a random screen
+
+import gaiatest
+import marionette
+import random
+import sys
+import time
+
+APP_LAUNCH_TIMEOUT = 0.5
+IGNORE_APPS_NAMES = []
+IGNORE_APPS_NAMES.append('Dev')  # new app in v1.3
+IGNORE_APPS_NAMES.append('Marketplace')
+IGNORE_APPS_NAMES.append('Membuster')
+IGNORE_APPS_NAMES.append('Mochitest')
+IGNORE_APPS_NAMES.append('PackStubTest')
+IGNORE_APPS_NAMES.append('Stage')  # new app in v1.3
+IGNORE_APPS_NAMES.append('Test Agent')
+IGNORE_APPS_NAMES.append('Test Container')
+IGNORE_APPS_NAMES.append('Test OTASP')
+IGNORE_APPS_NAMES.append('Test receiver#1')
+IGNORE_APPS_NAMES.append('Test Receiver#2')
+IGNORE_APPS_NAMES.append('Test receiver (inline)')
+IGNORE_APPS_NAMES.append('Test Wap Push')  # new app in v1.3
+IGNORE_APPS_NAMES.append('Usage')
+cookie = 'REMOV'
+cookie += 'EME    '
+
+
+def demo_shuffling_opening_non_opened_apps(apps, installed_apps, running_apps):
+    count = 1
+    random.shuffle(installed_apps)
+    print installed_apps
+    for app_name in installed_apps:
+        #import pdb; pdb.set_trace()
+        if app_name not in IGNORE_APPS_NAMES and app_name not in running_apps and count <= 3:  # launch 3 apps at a time
+            print str(count) + ': ' + app_name
+            apps.launch(app_name)
+            time.sleep(APP_LAUNCH_TIMEOUT)
+            running_apps.append(app_name)
+            count += 1
+    return running_apps
+
+
+def demo_non_stop_launching_apps(apps, installed_apps):
+    count = 1
+    while True:
+        app_name = random.choice(installed_apps)
+        if app_name not in IGNORE_APPS_NAMES:
+            count += 1
+            print str(count) + ' Opening: ' + app_name
+            print_frc_lines('apps.launch("' + app_name + '")')
+            apps.launch(app_name)
+            time.sleep(APP_LAUNCH_TIMEOUT)
+
+
+def get_installed_app_names(apps):
+    '''
+    Returns a list of names of installed apps.
+    '''
+    names = []
+    installedApps = apps.installed_apps
+    for index in xrange(len(installedApps)):
+        names.append(installedApps[index].name)
+    return names
+
+
+def get_running_app_names(apps):
+    '''
+    Returns a list of names of running apps.
+    '''
+    names = []
+    runningApps = apps.running_apps
+    for index in xrange(len(runningApps)):
+        names.append(runningApps[index].name)
+    return names
+
+
+def print_frc_lines(line):
+    print cookie + line
+
+
+def main():
+    fuzzSeed = time.time()
+    random.seed(fuzzSeed)
+    print 'fuzzSeed is: ' + str(fuzzSeed)
+
+    mari = marionette.Marionette()
+    mari.start_session()
+    apps = gaiatest.GaiaApps(mari)
+    # The following 2 lines used to work in 1.2 and 1.3 B2G Desktop/emulators, but not yet the Flame
+    #lock = gaiatest.LockScreen(mari)
+    #lock.unlock()
+
+    installed_apps = get_installed_app_names(apps)
+    print installed_apps
+    running_apps = get_running_app_names(apps)
+    print running_apps
+
+    start(apps, installed_apps, running_apps)
+
+
+def start(apps, installed_apps, running_apps):
+    # DDBEGIN
+    #demo_shuffling_opening_non_opened_apps(apps, installed_apps, running_apps)
+    demo_non_stop_launching_apps(apps, installed_apps)
+    # DDEND
+
+main()
